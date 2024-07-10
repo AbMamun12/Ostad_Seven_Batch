86ae9e154584349bcce588f2d8a80ad5bc83f85f
AbMamun12 <abdullah.mamun8191@gmail.com>
2024-07-10 17:08:04 +0600

    new

---
+++ bin/Null_Safety.dart
@@ -0,0 +1,29 @@
+void main (){
+  // ? ?? ! এগুলোকে বলা হয় নাল অঅপারেতটর। ডার্ট নাল সেফটি নিয়ে কাজ করে।
+  // ? মানে হলো হয় এটা নয়তো ওটা ?? মানে হলো বামপাশ এর টা ! এটা মানে হলো ( সন্দেহ আছে )
+  // একটি প্রোগ্রাম হয় নাল হবে নয়তো valid হবে।
+  // নিচের লাইনের মানে হলো  int type হবে নয়তো null type হবে।
+  int ? myage= null;
+  // my age =34;
+  //null
+  print(myage);
+  print(myage.runtimeType);
+
+   myage =34;
+  //null
+  print(myage);
+  print(myage.runtimeType);
+
+  String ? name;
+  /*if (name == null){
+    print('hello');
+  }
+  else{
+    print(name);
+  }*/
+  print(name??'hello');
+  print(name);
+  name= 'abdullah';
+  print(name);
+
+}
