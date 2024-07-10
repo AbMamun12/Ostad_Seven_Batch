d3b47fb9ec11d1ec1881d4d803b0bb322b9814e6
AbMamun12 <abdullah.mamun8191@gmail.com>
2024-07-10 13:39:13 +0600

    new

--- bin/for_loop.dart
+++ bin/for_loop.dart
@@ -1,3 +1,5 @@
+//import 'dart:io';
+
 void main(){
   //loop
   //for, while, do-while
@@ -16,6 +18,62 @@ void main(){
 print(i);
 greeting();
 }
+  // While লুপ এর জন্য
+  int h=50;
+  while(h<=55){
+    print(h);
+    h++;
+    //এখানে greeting মানে হলো সা্গতমসূচক একটি ফাংশন যজেনা রে কে এখানে কল করেচি
+    greeting();
+  }
+  // list কে যদি ফর লুপ দিয়ে প্রিন্ট করতে চাই,তবে করা যায়
+  List<String> students =[
+    'Abdullah',
+    'Al',
+    'Mamun',
+    'Ismail',
+    'Fahim',
+    'Shakil',
+    'Zihad',
+  ];
+for(int i=0;i<students.length;i++)
+  {
+    print('studdents $i : ${students[i]}');
+  }
+// চাইলে For in ইউজ করে একই কাজ সহজে করতে পারি
+ for (String student in students){
+   print('Student name : $student');
+ }
+  // চাইলে For each ইউজ করে একই কাজ সহজে করতে পারি
+  students.forEach((stu){
+    print('Student name : $stu');
+  });
+
+// Map কেও For Loop দিয়ে হ্যান্ডল করা যায়
+  // এটা একটি ম্যাপ এর মধ্যে আরেকটি ম্যাপ
+ Map<String, Map<String, String>> friends ={
+   'abdullah': {
+     'address': 'Dhaka',
+     'age' : '30',
+   },
+   'mamun': {
+     'address': 'Bogura',
+     'age' : '40',
+   },
+   'almamun': {
+     'address': 'Jamalpur',
+     'age' : '23',
+   },
+ };
+
+ for(String key in friends.keys){
+   print( 'My friened name is $key. Address : ${friends[key]!['address']}   age : ${friends[key]!['age']}');
+ }
+ // আর শুধুই ভালু গুলো প্রিন্ট করতে চাইলে
+  for (Map<String,String>details in friends.values){
+    print(details);
+  }
+
 }
 void  greeting(){
   print('Welcome to home');
