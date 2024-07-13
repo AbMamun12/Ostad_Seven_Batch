b63e5c8eea11ac0a9534c8157f61528c49f1e075
AbMamun12 <abdullah.mamun8191@gmail.com>
2024-07-13 16:46:54 +0600

    new

--- OOP_Module_3/3.human.dart
+++ OOP_Module_3/3.human.dart
@@ -1,49 +1,71 @@
-void main(){
-// Data-Type variable-name
-// class-Human
-//class-name name = class-name()
-
-Human rahim = Human('Abdullah');
-rahim.eating();
-rahim.moving();
-rahim.talking('Tamim');
-
-// change করতে চাইলে
- Human karim = Human('Karim mia');
- print(karim.name);
-
-
-
-
-}
-class Human{
-//attributes
-  String name ='rahim';
-  int age =23;
-  String address ='Dhaka';
-
-
- // Dafult constructor
-  //Human(){
-  //}
-  Human(String username){
-    print('Creating a new Human $username');
-    name= username;
-    age= userAge;
-    address= userAddress;
-
-  }
-
-
-  void moving (){
-    print('$name is moving');
-  }
-  void eating (){
-    print('$name is eating');
-  }
-  void talking (String talkingwith){
-    print('$name is talking with $talkingwith');
-  }
-
-
-}
+// void main(){
+// // Data-Type variable-name
+// // class-Human
+// //class-name name = class-name()
+//
+// Human rahim = Human('Abdullah', 6, 'Dhaka');
+// rahim.eating();
+// rahim.moving();
+// rahim.talking('Tamim');
+//
+// // change করতে চাইলে
+//  Human karim = Human('Karim mia',345,'Borishal');
+//  print(karim.name);
+//  print(karim.address);
+//  print(karim.age);
+//  //karim.name ='karim mia';
+//
+// Employee mamun= Employee('mamun', 'Bogura, Bangladesh', 'Absoftware LTD', 23, 3000000);
+//
+//
+// }
+// class Human{
+// //attributes
+//   String name ='';
+//   int age =0;
+//   String address ='';
+//
+//
+//  // Dafult constructor
+//   //Human(){
+//   //}
+//   Human(String username,int userAge, String userAddress){
+//     print('Creating a new Human $username');
+//     name= username;
+//
+//     age= userAge;
+//     address= userAddress;
+//
+//   }
+//
+//
+//   void moving (){
+//     print('$name is moving');
+//   }
+//   void eating (){
+//     print('$name is eating');
+//   }
+//   void talking (String talkingwith){
+//     print('$name is talking with $talkingwith');
+//   }
+//
+//
+// }
+// // child-name extends parents-name/class-base
+// // একই কাজ দুইবার না করে কোড টাকে reuse   করে ব্যবহার করা।
+// class Employee extends Human{
+//   late String name;
+//   late String address;
+//   late int age;
+//   late String organizaton;
+//   late double salary;
+//
+// //
+// //   Employee(String employeeName,String employeeAddress, String employeeOrganization, int employeeAge, double employeeSalary){
+// //     this.age=employeeAge;
+// //     this.name =employeeName;
+// //     this.address=employeeAddress;
+// //     this.organizaton=employeeOrganization;
+// //     this.salary=employeeSalary;
+// //   }
+// // } ভিডিও এর লাস্ট ৩০ মিনিট ক্লিয়ার করতে হবে
